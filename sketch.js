ec59b72b73df959ecf2b924e47ce8bfe0b9ea33b
Kryzor <116125485+Kryzor@users.noreply.github.com>
2025-06-03 21:28:52 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,7 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(255,0,0);
 }
